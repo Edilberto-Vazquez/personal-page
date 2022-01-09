e299a6229a4f8bfc11e78212f5d60135de0337cf
Edilberto <edi_vazquez12@hotmail.com>
2022-01-09 00:53:01 -0600

    navbar responsive with some styles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./pages/App";
+import App from "./pages/Home";
+import "./styles/index.scss";
 ReactDOM.render(<App />, document.getElementById("app"));
--- src/pages/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../styles/global.scss";
-
-const App = () => {
-  return <div>Hello world</div>;
-};
-
-export default App;
