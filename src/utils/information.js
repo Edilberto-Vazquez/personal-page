14c8ffa474bf7b3a09cc090e0c44910b1781e0f1
Edilberto <edi_vazquez12@hotmail.com>
2022-01-15 22:28:08 -0600

    change positon in what i do

--- src/utils/information.js
+++ src/utils/information.js
@@ -26,13 +26,13 @@ const WhatIDoList = [
       "In the backend I like to develop with NodeJS and Express and with Golang I like to use Gin as framework, ",
   },
   {
-    icon: "programminglanguages",
+    icon: "languages",
     title: "Programming Languages",
     description:
       "the main languajes that I use are JavaScript Golang and Python, and I have basic knowledge in Java, TypeScrip and soon I will be learning Rust and SQL for DataBases",
   },
   {
-    icon: "tecnologies",
+    icon: "technology",
     title: "Tecnologies",
     description:
       "With operative system I like to use linux the distribution that I currently use is Ubuntu with WSL, I use git as a version manager and GitHub as repository for my personal projects and for third party projects I use GitLab, The main data bases that I use are MySQL, PostgreSQL, and MongoDB and I have basic kwoledge with AWS",
